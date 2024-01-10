36a5bc71c2eabdb03de015484a758552b6d95c8e
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-01-10 17:29:34 +0700

    Connection

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\SQLServerConnectionController;
+
+Route::get('/check-sql-connection', [SQLServerConnectionController::class, 'checkConnection']);
+
