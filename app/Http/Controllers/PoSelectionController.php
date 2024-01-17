e05e70555103c947489520e6797b419505273b61
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-01-17 22:54:29 +0700

    PO

--- app/Http/Controllers/PoSelectionController.php
+++ app/Http/Controllers/PoSelectionController.php
@@ -43,7 +43,7 @@ class PoSelectionController extends Controller
             'user_name'     => $data["user_name"],
             'url_file'      => $url_data,
             'file_name'     => $file_data,
-            'module'        => "PoSelection",
+            'module'        => "Selection",
             'body'          => "Please approve Quotation No. ".$data['po_doc_no']." for ".$po_descs,
             'subject'       => "Need Approval for Quotation No.  ".$data['po_doc_no'],
         );
