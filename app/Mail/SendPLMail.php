7665001660f14e27bc12431898628558e023cfdc
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-01-18 19:05:31 +0700

    PL Lyman

---
+++ app/Mail/SendPLMail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendPLMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $encryptedData;
+    public $dataArray;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param array $encryptedData
+     * @param array $dataArray
+     * @return void
+     */
+    public function __construct($encryptedData, $dataArray)
+    {
+        $this->encryptedData = $encryptedData;
+        $this->dataArray = $dataArray;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+
+        return $this->subject($this->dataArray['subject'])
+                    ->view('email.plbudget.send')
+                    ->with([
+                        'encryptedData' => $this->encryptedData,
+                        'dataArray' => $this->dataArray,
+                    ]);
+    }
+}
