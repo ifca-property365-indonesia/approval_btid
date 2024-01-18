b2e7a93598ff20d5742bd90f3f3f9c67cec57ed0
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-01-18 11:24:09 +0700

    Cm Progress

---
+++ app/Http/Controllers/CmProgressController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use App\Mail\SendCmEntryMail;
+use PDO;
+use DateTime;
+
+class CmProgressController extends Controller
+{
+    public function processModule($data)
+    {
+        $dataArray = array(
+            'sender'        => $data["sender"],
+            'entity_name'   => $data["entity_name"],
+            'descs'         => $data["descs"],
+            'user_name'     => $data["user_name"],
+            'module'        => $data["module"],
+            'body'          => "Please approve Contract Progress No. ".$data['doc_no']." for ".$data["descs"],
+            'subject'       => "Need Approval for Contract Progress No.  ".$data['doc_no'],
+        );
+
+        $data2Encrypt = array(
+            'entity_cd'     => $data["entity_cd"],
+            'project_no'    => $data["project_no"],
+            'email_address' => $data["email_addr"],
+            'level_no'      => $data["level_no"],
+            'doc_no'        => $data["doc_no"],
+            'ref_no'        => $data["ref_no"],
+            'usergroup'     => $data["usergroup"],
+            'user_id'       => $data["user_id"],
+            'supervisor'    => $data["supervisor"],
+            'type'          => 'A',
+            'type_module'   => 'CM',
+            'text'          => 'Contract Progress'
+        );
+
+        
+
+        // Melakukan enkripsi pada $dataArray
+        $encryptedData = Crypt::encrypt($data2Encrypt);
+    
+        try {
+            $emailAddresses = $data["email_addr"];
+        
+            // Check if email addresses are provided and not empty
+            if (!empty($emailAddresses)) {
+                $emails = is_array($emailAddresses) ? $emailAddresses : [$emailAddresses];
+                
+                foreach ($emails as $email) {
+                    Mail::to($email)->send(new SendCmEntryMail($encryptedData, $dataArray));
+                }
+                
+                $sentTo = is_array($emailAddresses) ? implode(', ', $emailAddresses) : $emailAddresses;
+                Log::channel('sendmail')->info('Email berhasil dikirim ke: ' . $sentTo);
+                return "Email berhasil dikirim ke: " . $sentTo;
+            } else {
+                Log::channel('sendmail')->warning('Tidak ada alamat email yang diberikan.');
+                return "Tidak ada alamat email yang diberikan.";
+            }
+        } catch (\Exception $e) {
+            Log::channel('sendmail')->error('Gagal mengirim email: ' . $e->getMessage());
+            return "Gagal mengirim email: " . $e->getMessage();
+        }
+    }
+
+    public function update($status, $encrypt, $reason)
+    {
+        $data = Crypt::decrypt($encrypt);
+
+        if ($status == "A") {
+            $descstatus = "Approved";
+            $imagestatus = "approved.png";
+        } else if ($status == "R") {
+            $descstatus = "Revised";
+            $imagestatus = "revise.png";
+        } else {
+            $descstatus = "Cancelled";
+            $imagestatus = "reject.png";
+        }
+        $pdo = DB::connection('BTID')->getPdo();
+        $sth = $pdo->prepare("SET NOCOUNT ON; EXEC xrl_send_mail_approval_cm_progress ?, ?, ?, ?, ?, ?, ?, ?, ?, ?;");
+        $sth->bindParam(1, $data["entity_cd"]);
+        $sth->bindParam(2, $data["project_no"]);
+        $sth->bindParam(3, $data["doc_no"]);
+        $sth->bindParam(4, $data["ref_no"]);
+        $sth->bindParam(5, $status);
+        $sth->bindParam(6, $data["level_no"]);
+        $sth->bindParam(7, $data["usergroup"]);
+        $sth->bindParam(8, $data["user_id"]);
+        $sth->bindParam(9, $data["supervisor"]);
+        $sth->bindParam(10, $reason);
+        $sth->execute();
+        if ($sth == true) {
+            $msg = "You Have Successfully ".$descstatus." the Contract Progress No. ".$data["doc_no"];
+            $notif = $descstatus." !";
+            $st = 'OK';
+            $image = $imagestatus;
+        } else {
+            $msg = "You Failed to ".$descstatus." the Contract Progress No.".$data["doc_no"];
+            $notif = 'Fail to '.$descstatus.' !';
+            $st = 'OK';
+            $image = "reject.png";
+        }
+        $msg1 = array(
+            "Pesan" => $msg,
+            "St" => $st,
+            "notif" => $notif,
+            "image" => $image
+        );
+        return view("email.after", $msg1);
+    }
+}
