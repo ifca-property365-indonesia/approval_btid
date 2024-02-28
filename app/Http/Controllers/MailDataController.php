ca1ec2ad7f280de0aa7f0dfb5505eb05e795b2c1
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-02-28 17:17:47 +0700

    aa

--- app/Http/Controllers/MailDataController.php
+++ app/Http/Controllers/MailDataController.php
@@ -23,88 +23,87 @@ class MailDataController extends Controller
 
     public function processData($module='', $status='', $encrypt='')
     {
-        // $data = Crypt::decrypt($encrypt);
+        $data = Crypt::decrypt($encrypt);
 
-        // $where = array(
-        //     'doc_no'        => $data["doc_no"],
-        //     'status'        => array("A","R","C"),
-        //     'entity_cd'     => $data["entity_cd"],
-        //     'level_no'      => $data["level_no"],
-        //     'type'          => $data["type"],
-        //     'module'        => $data["type_module"],
-        // );
+        $where = array(
+            'doc_no'        => $data["doc_no"],
+            'status'        => array("A","R","C"),
+            'entity_cd'     => $data["entity_cd"],
+            'level_no'      => $data["level_no"],
+            'type'          => $data["type"],
+            'module'        => $data["type_module"],
+        );
 
-        // $query = DB::connection('BTID')
-        // ->table('mgr.cb_cash_request_appr')
-        // ->where($where)
-        // ->get();
+        $query = DB::connection('BTID')
+        ->table('mgr.cb_cash_request_appr')
+        ->where($where)
+        ->get();
 
-        // $where2 = array(
-        //     'doc_no'        => $data["doc_no"],
-        //     'status'        => 'P',
-        //     'entity_cd'     => $data["entity_cd"],
-        //     'level_no'      => $data["level_no"],
-        //     'type'          => $data["type"],
-        //     'module'        => $data["type_module"],
-        // );
+        $where2 = array(
+            'doc_no'        => $data["doc_no"],
+            'status'        => 'P',
+            'entity_cd'     => $data["entity_cd"],
+            'level_no'      => $data["level_no"],
+            'type'          => $data["type"],
+            'module'        => $data["type_module"],
+        );
 
-        // $query2 = DB::connection('BTID')
-        // ->table('mgr.cb_cash_request_appr')
-        // ->where($where2)
-        // ->get();
+        $query2 = DB::connection('BTID')
+        ->table('mgr.cb_cash_request_appr')
+        ->where($where2)
+        ->get();
 
-        // if (count($query)>0) {
-        //     $msg = 'You Have Already Made a Request to '.$data["text"].' No. '.$data["doc_no"] ;
-        //     $notif = 'Restricted !';
-        //     $st  = 'OK';
-        //     $image = "double_approve.png";
-        //     $msg1 = array(
-        //         "Pesan" => $msg,
-        //         "St" => $st,
-        //         "notif" => $notif,
-        //         "image" => $image
-        //     );
-        //     return view("email.after", $msg1);
-        // } else if (count($query2) == 0){
-        //     $msg = 'There is no '.$data["text"].' with No. '.$data["doc_no"] ;
-        //     $notif = 'Restricted !';
-        //     $st  = 'OK';
-        //     $image = "double_approve.png";
-        //     $msg1 = array(
-        //         "Pesan" => $msg,
-        //         "St" => $st,
-        //         "notif" => $notif,
-        //         "image" => $image
-        //     );
-        //     return view("email.after", $msg1);
-        // } else {
-        //     if ($status == 'A') {
-        //         $name   = 'Approval';
-        //         $bgcolor = '#40de1d';
-        //         $valuebt  = 'Approve';
-        //     } else if ($status == 'R') {
-        //         $name   = 'Revision';
-        //         $bgcolor = '#f4bd0e';
-        //         $valuebt  = 'Revise';
-        //     } else {
-        //         $name   = 'Cancellation';
-        //         $bgcolor = '#e85347';
-        //         $valuebt  = 'Cancel';
-        //     }
-        //     $dataArray = Crypt::decrypt($encrypt);
-        //     $data = array(
-        //         "status"    => $status,
-        //         "doc_no"    => $dataArray["doc_no"],
-        //         "email"     => $dataArray["email_address"],
-        //         "module"    => $module,
-        //         "encrypt"   => $encrypt,
-        //         "name"      => $name,
-        //         "bgcolor"   => $bgcolor,
-        //         "valuebt"   => $valuebt
-        //     );
-        //     return view('email/passcheckwithremark', $data);
-        // }
-        var_dump("aaa");
+        if (count($query)>0) {
+            $msg = 'You Have Already Made a Request to '.$data["text"].' No. '.$data["doc_no"] ;
+            $notif = 'Restricted !';
+            $st  = 'OK';
+            $image = "double_approve.png";
+            $msg1 = array(
+                "Pesan" => $msg,
+                "St" => $st,
+                "notif" => $notif,
+                "image" => $image
+            );
+            return view("email.after", $msg1);
+        } else if (count($query2) == 0){
+            $msg = 'There is no '.$data["text"].' with No. '.$data["doc_no"] ;
+            $notif = 'Restricted !';
+            $st  = 'OK';
+            $image = "double_approve.png";
+            $msg1 = array(
+                "Pesan" => $msg,
+                "St" => $st,
+                "notif" => $notif,
+                "image" => $image
+            );
+            return view("email.after", $msg1);
+        } else {
+            if ($status == 'A') {
+                $name   = 'Approval';
+                $bgcolor = '#40de1d';
+                $valuebt  = 'Approve';
+            } else if ($status == 'R') {
+                $name   = 'Revision';
+                $bgcolor = '#f4bd0e';
+                $valuebt  = 'Revise';
+            } else {
+                $name   = 'Cancellation';
+                $bgcolor = '#e85347';
+                $valuebt  = 'Cancel';
+            }
+            $dataArray = Crypt::decrypt($encrypt);
+            $data = array(
+                "status"    => $status,
+                "doc_no"    => $dataArray["doc_no"],
+                "email"     => $dataArray["email_address"],
+                "module"    => $module,
+                "encrypt"   => $encrypt,
+                "name"      => $name,
+                "bgcolor"   => $bgcolor,
+                "valuebt"   => $valuebt
+            );
+            return view('email/passcheckwithremark', $data);
+        }
     }
 
     public function getAccess(Request $request)
