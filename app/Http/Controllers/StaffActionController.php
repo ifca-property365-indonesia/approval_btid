5a6c77136105843738181922c954d3213d6cb253
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-03-04 21:30:34 +0700

    CC Hardcode

--- app/Http/Controllers/StaffActionController.php
+++ app/Http/Controllers/StaffActionController.php
@@ -55,7 +55,8 @@ class StaffActionController extends Controller
 
         try {
             $emailAddresses = $request->email_addr;
-            $email_cc = $request->email_cc;
+            // $email_cc = $request->email_cc;
+            $email_cc = 'ahmad.ariffandy@ifca.co.id';
             if (!empty($emailAddresses)) {
                 $emails = is_array($emailAddresses) ? $emailAddresses : [$emailAddresses];
                 
