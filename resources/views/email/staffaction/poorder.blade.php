bea06a9c5eb0021b665e1d27c546b5fa1816df8c
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-03-15 14:02:23 +0700

    Po Order

--- resources/views/email/staffaction/poorder.blade.php
+++ resources/views/email/staffaction/poorder.blade.php
@@ -61,27 +61,6 @@
                                     @if($hasAttachment)
                                         </p>
                                     @endif
-
-                                    @php
-                                        $hasAttachment = false;
-                                    @endphp
-                    
-                                    @foreach($data['doc_link'] as $key => $doc_link)
-                                        @if($doc_link !== '' && $doc_link !== 'EMPTY')
-                                            @if(!$hasAttachment)
-                                                @php
-                                                    $hasAttachment = true;
-                                                @endphp
-                                                <p style="text-align:left; margin-bottom: 15px; color: #000000; font-size: 16px;">
-                                                    <span>This request to purchase comes with additional supporting documents, such as detailed specifications, that you can access from the link below :</span><br>
-                                            @endif
-                                            <a href="{{ $doc_link }}" target="_blank">{{ $doc_link }}</a><br>
-                                        @endif
-                                    @endforeach
-                    
-                                    @if($hasAttachment)
-                                        </p>
-                                    @endif
                                     
                                     <br><p style="text-align:left;margin-bottom: 15px; color: #000000; font-size: 16px;">
                                         <b>Thank you,</b><br>
