dbf43470b5f123b95574d1ff2147c332a09561db
fandynasution <ahmad.ariffandy@ifca.co.id>
2024-04-08 12:15:53 +0800

    curr_cd

---
+++ app/Http/Controllers/EncryptController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Storage;
+use App\Mail\SendPoSMail;
+use App\Mail\FeedbackMail;
+use App\Mail\StaffActionMail;
+use App\Mail\StaffActionPoRMail;
+use App\Mail\StaffActionPoSMail;
+use Carbon\Carbon;
+use PDO;
+use DateTime;
+
+
+class EncryptController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
